183e34b40e006dda85b71f2b8d8c332df86f2467
kohanman <kohanman@googlemail.com>
2025-10-25 00:11:27 +0100

    added function

--- prep/mean.js
+++ prep/mean.js
@@ -1,2 +1,11 @@
 const items = [4.6, 5.03, 7.99, 8.01];
 console.log(items[0]);
+
+function calculateMean(list) {
+  let total = 0;
+  for (const items of list) {
+    total += items;
+    console.log(total);
+  }
+  console.log(calculateMean(items));
+}

bc3c97de7ac1c898156da9a01202d0d66ee4adac
kohanman <kohanman@googlemail.com>
2025-10-25 02:17:34 +0100

    changed const to let

--- prep/swapping.js
+++ prep/swapping.js
@@ -2,8 +2,8 @@
 //   and swaps the first element with the last element
 
 function swapFirstAndLast(arr) {
-  const first = arr[0];
-  const last = arr[arr.length - 1];
+  let first = arr[0];
+  let last = arr[arr.length - 1];
   arr[0] = last;
   arr[arr.length - 1] = first;
 }

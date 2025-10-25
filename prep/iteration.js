d9d2d25b4e07ca65d80e9ebe41c65a94e95144fc
kohanman <kohanman@googlemail.com>
2025-10-25 01:21:59 +0100

    added to median test no sure if any tests working whatsoever, terminal is constantly giving errors even when i copy exactly from course notes

--- prep/iteration.js
+++
@@ -1,9 +0,0 @@
-function calculateMean(list) {
-  let total = 0;
-
-  for (const item of list) {
-    total += item;
-  }
-  return total / list.length;
-}
-console.log(calculateMean([10, 20, 30, 40, 50]));
---
+++ prep/prep.js
@@ -0,0 +1,4 @@
+const list = [10, 20, 30];
+const copy = [10, 20, 30];
+
+console.log(list === copy); // logs false

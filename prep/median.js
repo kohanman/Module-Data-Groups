7ce45d2887082cf1201f19a3a37581e672809c19
kohanman <kohanman@googlemail.com>
2025-10-25 01:45:20 +0100

    made median.js work with even numbers

--- prep/median.js
+++ prep/median.js
@@ -1,8 +1,12 @@
 function calculateMedian(list) {
   const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-
-  return median;
+  if (list.length % 2 === 0) {
+    const median =
+      (list.slice(middleIndex)[0] + list.slice(middleIndex - 1)[0]) / 2;
+    return median;
+  } else {
+    const median = list.slice(middleIndex)[0];
+    return median;
+  }
 }
 console.log(calculateMedian([10, 20, 30, 50, 60]));
-module.exports = { calculateMedian };
